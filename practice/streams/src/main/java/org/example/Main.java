be1f736a9e105637c048caadb89c0d87f03bd2b2
semen.antufiev <alexantufiev@gmail.com>
2025-03-09 14:45:58 +0300

    create practice stream

---
+++ practice/streams/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        final Test1 test1 = new Test1();
+        test1.case1();
+        System.out.println("next case");
+        test1.case2();
+        System.out.println("next case");
+        test1.case3();
+        System.out.println("next case");
+        test1.case4();
+        System.out.println("next case");
+        test1.case5();
+
+    }
+}
